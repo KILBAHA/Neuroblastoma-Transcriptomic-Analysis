83a85a89a82c3bb81f90b6319783cdcb758ac8d0
Yusuf <yusufaahmed@outlook.com>
2021-02-02 00:10:30 +0000

    added directed ML

---
+++ directed_classification/preprocess_directed_classification.R
@@ -0,0 +1,37 @@
+
+### Read in z_scores and format ###
+z_scores = read.csv("data_RNA_Seq_mRNA_median_all_sample_Zscores.txt", sep = "\t", stringsAsFactors = FALSE, header = TRUE)
+
+map = z_scores[,c(1,2)]
+
+inst_names = colnames(z_scores)[3:length(colnames(z_scores))]
+
+z_scores = z_scores[,-2]
+z_scores = as.data.frame(t(z_scores), stringsAsFactors = FALSE)
+names(z_scores) = map[,1]
+z_scores = z_scores[-1,]
+
+z_scores = as.data.frame(apply(z_scores, 2, as.numeric))
+
+name_fix = c()
+for(name in inst_names){name_fix = c(name_fix,substr(name,11,nchar(name)-3))}
+
+
+row.names(z_scores) = name_fix
+
+## Read in Clinical Data ###
+patient = as.data.frame(read.csv("data_clinical_patient.txt", sep = "\t", stringsAsFactors = FALSE, header = TRUE, skip = 4))
+p_nam = patient[,1]
+
+p_nam_fix = c()
+for(name in p_nam){p_nam_fix = c(p_nam_fix,substr(name,11,nchar(name)))}
+
+patient = patient[,2:ncol(patient)]
+
+row.names(patient) = p_nam_fix
+
+status = data.frame(patient[,"OS_STATUS"])
+colnames(status) = "STATUS"
+rownames(status) = p_nam_fix
+
+mg = merge.data.frame(status,z_scores, by="row.names")
